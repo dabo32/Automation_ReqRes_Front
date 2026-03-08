2df71ac5219d614953c88cda52de474d02078fbf
David Santiago <dasabo30@gmail.com>
2026-03-08 18:32:02 -0500

    feat: add question to validate authentication error messages

---
+++ src/test/java/com/saucedemo/automation/questions/TheErrorMessage.java
@@ -0,0 +1,17 @@
+package com.saucedemo.automation.questions;
+
+import com.saucedemo.automation.userinterfaces.LoginPage;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.questions.Text;
+
+public class TheErrorMessage implements Question<String> {
+    public static TheErrorMessage value() {
+        return new TheErrorMessage();
+    }
+
+    @Override
+    public String answeredBy(Actor actor) {
+        return Text.of(LoginPage.ERROR_MESSAGE).answeredBy(actor);
+    }
+}
